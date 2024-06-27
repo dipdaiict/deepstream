cade6825a48052e66e47c023860c6bcd9eaf8338
Dip Patel <dippatel256@gmail.com>
2024-06-27 18:38:12 +0530

    folder name changed.

--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
 from typing import List
 from setuptools import find_packages, setup
 
-HYPEN_E_DOT = '-e .'
+HYPHEN_E_DOT = '-e .'
 
-def get_requirements(file_path: str) -> List[str]:
-    requirements = []
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
     return requirements
 
 setup(
-    name="DeepStream", 
+    name="DeepStream",
     version="0.0.1",
     author="Dip Patel",
     author_email="dippatel256@gmail.com",
-    description="An end-to-end deep learning project", 
+    description="An end-to-end deep learning project",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url="https://github.com/dipdaiict/deepstream", 
+    url="https://github.com/dipdaiict/deepstream",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     packages=find_packages(),
-    install_requires=get_requirements('requirements_dev.txt'),  # Assuming requirements file is in the same directory
-    python_requires='>=3.12',  # Specify the Python version required
+    install_requires=get_requirements(r'C:\Self-Learning\deepstream\requirements_dev.txt'),
+    python_requires='>=3.12',
 )
